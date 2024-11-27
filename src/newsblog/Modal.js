a72c274624092616d1148b3ca1716d89c0f4bd7c
psm0419 <sm8804@naver.com>
2024-11-27 16:24:56 +0900

    발행, 삭제 기능 추가

--- src/newsblog/Modal.js
+++ src/newsblog/Modal.js
@@ -8,15 +8,15 @@ import NewsBlog from "./NewsBlog";
 function Modal(props) {  
     return (
         <div className="modal" style = {{backgroundColor:props.bgColor}}>
-            <h3>제목</h3>
+            <h3>{props.title} ❤ {props.like}</h3>
             <p>날짜</p>
             <p>상세내용</p>
-            <button onClick={()=>{
+            {/* <button onClick={()=>{
                 let temp = [...props.news];
                 temp[0] = '긴급 뉴스';
                 props.setNews(temp);
                 
-            }}>제목 수정</button>
+            }}>제목 수정</button> */}
         </div>
     )
 }
--- src/newsblog/NewsBlog.js
+++ src/newsblog/NewsBlog.js
@@ -18,6 +18,12 @@ function NewsBlog(props) {
 
     let [flag, setFlag] = useState(false);
 
+    let [selectedTitle, setSelectedTitle] = useState('');
+
+    let [likeSave, setLikeSave] = useState(0);
+
+    let [inputText, setInputText] = useState('');
+
     return (
         <div>
             <div className='black-nav'>
@@ -28,23 +34,29 @@ function NewsBlog(props) {
                 news.map((item, index) => {
                     return (
                         <div className='post-list'>
-                            <h4><span onClick={() => {
-                                flag == true ? setFlag(false) : setFlag(true)
+                            <h4><span onClick={() => { //제목 클릭
+                                flag == true ? setFlag(false) : setFlag(true) //모달 on/off
+                                //어떤뉴스를 눌렀는지 저장해야함
+                                setSelectedTitle(news[index]); //item 넣어도 같음
+                                setLikeSave(count[index]);
                             }}>{news[index]}</span>
                                 <span onClick={() => {
                                     let temp = [...count];
                                     temp[index]++;
-                                    setCount(temp); 
+                                    setCount(temp);
                                 }}> ❤ </span>{count[index]}
                             </h4>
                             <p>내용 무</p>
+                            <button onClick={()=>{
+                                let temp = [...news];
+                                temp.splice(index, 1);
+                                setNews(temp);
+                                count.splice(index, 1);
+                            }}>삭제</button>
                         </div>
                     )
                 })
             }
-
-
-
             {/* <div className='post-list'>
                 <h4><span onClick={() => {
                     flag == true ? setFlag(false) : setFlag(true)
@@ -87,7 +99,35 @@ function NewsBlog(props) {
                 // temp.splice(0, 1, 'Today News') //0인덱스부터 1개 삭제, 뒤에 추가
                 setNews(temp);
             }}>제목 변경</button>
-            {flag == true ? <Modal news={news} setNews={setNews} bgColor={'lightgreen'}/> : null}
+
+            <div>
+                <input type='text' id="input_news" value={inputText} onChange={(event)=>{
+                    //이벤트 등록해서 사용하는 방식
+                    setInputText(event.target.value);
+                }}></input>
+                <button onClick={()=>{
+                    //입력된 값 확인 -> news 배열에 추가저장
+
+                    //onChange 안쓰는 방법
+                    // let temp = [...news];
+                    // temp.push(document.getElementById('input_news').value);
+                    // setNews(temp);
+                    // document.getElementById('input_news').value = ""; //입력하고 공백
+                    inputText = inputText.trim();
+                    if(inputText != ''){ //다른방법: inputText.length == 0 , null , undefined
+                        //전제조건: 양측에 있는 띄어쓰기는 제외(trim)
+                        //trim은 비파괴적 메서드라 원본은 띄어쓰기 남아있음 밖에서 트림하고 들어와야함
+                        let temp = [...news];                        
+                        temp.push(inputText);
+                        setNews(temp);
+                        setInputText(''); //이거 쓰고 위에 value 만들어야 입력하고 공백됨
+                        // let tempCount = [...count]; 위에서 재렌더링 일어나니까 필요없음
+                        count.push(0); //위에 temp push랑 맞춰줘야 인덱스 맞음
+                    }
+                }}>발행</button>
+            </div>
+            {flag == true ? <Modal like={likeSave} title={selectedTitle} news={news} setNews={setNews} bgColor={'lightgreen'} /> : null}
+
 
         </div>
     )
